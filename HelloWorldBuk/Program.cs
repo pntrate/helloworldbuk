f79e886fa47fe0bd47d0c4a2fcee92e4599ea5c2
buko30 <giorgibukia31@gmail.com>
2024-01-11 16:01:18 +0400

    C# Sharp Basic Exercises - 103

--- HelloWorldBuk/Program.cs
+++ HelloWorldBuk/Program.cs
@@ -1,11 +1,41 @@
-﻿string vowels = "aeiouAEIOU";
-string s = Console.ReadLine();
-for (int i = 0; i < s.Length; i++)
+﻿//C# Sharp Basic Exercises
+
+using System.Text;
+
+static bool check(char a, char b)
 {
-    if (vowels.Contains(s[i]))
+    if (Char.ToLower(a) < Char.ToLower(b))
+    { return true; }
+    else { return false; }
+}
+
+string alphabet = Console.ReadLine();
+string numbers = "";
+StringBuilder characters = new StringBuilder();
+for (int i = 0; i < alphabet.Length; i++)
+{
+    if (Char.IsDigit(alphabet[i]))
     {
-        s = s.Remove(i, 1);
-        i--;
+        numbers += alphabet[i];
+    }
+    else
+    {
+        characters.Append(alphabet[i]);
     }
 }
-Console.WriteLine(s);
+for (int i = 0; i < characters.Length - 1; i++)
+{
+    for (int j = i + 1; j < characters.Length; j++)
+    {
+        if (!check(characters[i], characters[j]))
+        {
+            char y = characters[i];
+            characters[i] = characters[j];
+            characters[j] = y;
+        }
+    }
+}
+char[] chars = numbers.ToCharArray();
+Array.Sort(chars);
+Console.Write(characters);
+Console.Write(chars);
